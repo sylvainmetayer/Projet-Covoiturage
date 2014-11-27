1f943c39e394bd2709c8746f4cc712e5c3efe8a3
StarSlash <clement.patricio@archemy.fr>
2014-11-27 14:24:30 +0100

    Espace
    J'ai mis un espace

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -8,7 +8,7 @@
 	function addJours($date, $nbJours){
 		$membres = explode('/', $date);
 		$date = $membres[2].'-'.$membres[1].'-'.(intval($membres[0])+$nbJours);
-		return $date;
+		return $date; 
 	}
 	
 ?>
